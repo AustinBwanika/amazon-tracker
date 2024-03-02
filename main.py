f37cda17337304a379a101d644be1115480bdaf5
AustinBwanika <austin.kasekende12@gmail.com>
2024-03-02 14:25:40 +0000

    editing the password

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import html
 import os
 
 my_email = os.environ.get("EMAIL")
-# password = "nzqubvfhaptabwek"
-password = "djwfobssnqexzwwr"
+password = os.environ.get("PASSWORD")
 
 PRODUCT_URL = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
